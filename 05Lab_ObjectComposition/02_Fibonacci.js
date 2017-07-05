7aadffbb94ae88c4a559cbc00184396df34f9255
Martin <martin.p.nikolov@gmail.com>
2017-07-05 23:16:45 +0300

    05Lab ObjectComposition all problems solved

--- 05Lab_ObjectComposition/02_Fibonacci.js
+++ 05Lab_ObjectComposition/02_Fibonacci.js
@@ -16,40 +16,23 @@ function fibonacci(n) {
 }
 
 console.log(fibonacci(5));
-// function getFibonator() {
-//
-//
-//     let f0 = 0, f1 = 1;
-//
-//
-//     return function () {
-//
-//
-//         let f2 = f0 + f1;
-//
-//
-//         f0 = f1;
-//
-//
-//         f1 = f2;
-//
-//
-//         return console.log(f1);
-//
-//
-//     };
-//
-//
-// }
-//
-//
-// let fib = getFibonator();
-//
-//
-// fib(); // 1
-//
-//
-// fib(); // 1
-//
-//
-// fib(); // 2
+
+function getFibonator() {
+    let f1 = 0;
+    let f2 = 1;
+
+    return function () {
+        let f3 = f1 + f2;
+        f1 = f2;
+        f2 = f3;
+
+        return f1;
+    };
+}
+
+let fib = getFibonator();
+fib();
+fib();
+fib();
+fib();
+fib();
