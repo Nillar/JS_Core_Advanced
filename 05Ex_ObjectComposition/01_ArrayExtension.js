60bb06b469e107c5a6548a8aa222fe0a6bef6bf3
Martin <martin.p.nikolov@gmail.com>
2017-07-05 23:33:06 +0300

    05Ex ObjectComposition all problems solved

--- 05Ex_ObjectComposition/01_ArrayExtension.js
+++ 05Ex_ObjectComposition/01_ArrayExtension.js
@@ -1,3 +1,35 @@
-/**
- * Created by Martin on 5.7.2017.
- */
+(function solve() {
+    Array.prototype['last'] = function (n) {
+        return this[this.length - 1];
+    };
+
+    Array.prototype['skip'] = function (n) {
+        let result = [];
+        for (let i = n; i < this.length; i++) {
+            result.push(this[i]);
+        }
+        return result;
+    };
+
+    Array.prototype['take'] = function (n) {
+        let result = [];
+        for (let i = n; i < n; i++) {
+            result.push(this[i]);
+        }
+        return result;
+    };
+
+    Array.prototype['sum'] = function (n) {
+        let result = 0;
+        for (let i = n; i < this.length; i++) {
+            result +=this[i];
+        }
+        return result;
+    };
+
+    Array.prototype['average'] = function (n) {
+        return this.sum() / this.length;
+    };
+})()
+
+// solution();
--- 05Ex_ObjectComposition/04_ExtensibleObject.js
+++ 05Ex_ObjectComposition/04_ExtensibleObject.js
@@ -1,3 +1,15 @@
-/**
- * Created by Martin on 5.7.2017.
- */
+function solve() {
+    return {
+        extend: function (otherObj) {
+            let keys = Object.keys(otherObj);
+            for (let key of keys) {
+                let property = otherObj[key];
+                if (typeof property === 'function') {
+                    this.__proto__[key] = property;
+                } else {
+                    this[key] = property;
+                }
+            }
+        }
+    };
+}
